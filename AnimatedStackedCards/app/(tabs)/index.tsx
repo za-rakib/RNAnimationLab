5ea667efb0b0e9bea12b1a0f478e4c988795332e
Rakib <rakib@sayburgh.com>
2024-11-11 18:32:24 +0600

    animated card done

--- AnimatedStackedCards/app/(tabs)/index.tsx
+++ AnimatedStackedCards/app/(tabs)/index.tsx
@@ -1,12 +1,77 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
 const index = () => {
+  const progress = useSharedValue(0);
+
   return (
-    <View style={styles.container}>
+    <View
+      style={styles.container}
+      onTouchStart={() => {
+        progress.value = withSpring(1, { mass: 2 });
+      }}
+      onTouchEnd={() => {
+        progress.value = withSpring(0);
+      }}
+    >
       <StatusBar style="auto" />
-      <View style={styles.card}></View>
+
+      {new Array(4).fill(null).map((_, index) => {
+        const rStyle = useAnimatedStyle(() => {
+          const translateX = interpolate(
+            progress.value,
+            [0, 1],
+            [0, index * 20]
+          );
+          const translateY = interpolate(
+            progress.value,
+            [0, 1],
+            [0, index * -5]
+          );
+          const rotate = interpolate(
+            progress.value,
+            [0, 1],
+            [-index * 8, index * 8]
+          );
+          return {
+            transform: [
+              {
+                translateX: translateX, //progress.value * index * 20, //index * 20,
+              },
+              {
+                translateY: translateY, // progress.value * index * -5, // index * -5,
+              },
+              {
+                rotate: `${rotate}deg`, //`${ index * 8}deg`
+              },
+            ],
+          };
+        });
+        return (
+          <Animated.View
+            key={index}
+            style={[
+              styles.card,
+              rStyle,
+              {
+                zIndex: -index,
+                // transform: [
+                //   {
+                //     rotate: `${index * - 10}deg`,
+                //   },
+                // ],
+              },
+            ]}
+          ></Animated.View>
+        );
+      })}
     </View>
   );
 };
@@ -16,13 +81,23 @@ export default index;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#c8cce0",
+    backgroundColor: "#e3e3e3",
     justifyContent: "center",
     alignItems: "center",
   },
   card: {
     height: 180,
     aspectRatio: 3 / 4,
-    backgroundColor: "red",
+    backgroundColor: "white",
+    borderRadius: 25,
+    borderCurve: "continuous",
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 10,
+    elevation: 3,
+    borderWidth: 1,
+    borderColor: "#b9b9b9",
+    position: "absolute",
   },
 });
